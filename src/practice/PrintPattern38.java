cb38270e0c5b0b1224d1a698f79efbc63acc451d
Sheetal Gawande <sheetalgawande07@gmail.com>
2024-12-14 21:54:56 +0530

    PrintPattern change

--- src/practice/PrintPattern38.java
+++ src/practice/PrintPattern38.java
@@ -8,9 +8,9 @@ package practice;
 
 public class PrintPattern38 {
 	public static void main(String[] args) {
-		for(int i=1;i<=4;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print(" * ");
+		for(int i=1;i<=8;i++) {
+			for(int j=1;j<=7-i;j++) {
+				System.out.print(" & ");
 			}
 			System.out.println();
 			
