65de34cd8cd78106f11e5fd0b6355f6799c76f2f
Sheetal Gawande <sheetalgawande07@gmail.com>
2024-12-14 18:25:33 +0530

    PrintPattern

---
+++ src/practice/PrintPattern38.java
@@ -0,0 +1,23 @@
+/*Assignment 38 write a logic to print below pattern.
+*
+**
+***
+****
+*/
+package practice;
+
+public class PrintPattern38 {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+			
+			
+		}
+	}
+	
+}
+
+
